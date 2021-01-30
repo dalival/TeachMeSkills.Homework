cfbb0a8a3975f91ebb6434e11258b36ff29b3df5
Egor Fedorenko <egorfedorenko.w@gmail.com>
2021-01-30 03:16:59 +0300

    comments added

--- TMS.Net07.Homework.DaysOfWeek/Middle/Program.cs
+++ TMS.Net07.Homework.DaysOfWeek/Middle/Program.cs
@@ -23,14 +23,15 @@ namespace Middle
                 }
                 if (Regex.IsMatch(input, dateFormat))
                 {
+                    // I can use Parse instead of TryParse because of regex above
                     int day = int.Parse(input.Substring(0,2));
                     int month = int.Parse(input.Substring(3, 2));
                     int year = int.Parse(input.Substring(6));
+                    // regex doesn't work perfectly, for example user still can enter 39.10.2020. It'll cause exception. 
                     try
                     {
                         DateTime date = new DateTime(year, month, day);
-                        Console.WriteLine("\nIt's " + date.DayOfWeek
-                            + ". Enter another date or \"exit\" to exit.\n");
+                        Console.WriteLine("\nIt's " + date.DayOfWeek + ". Enter another date or \"exit\" to exit.\n");
                     }
                     catch
                     {
